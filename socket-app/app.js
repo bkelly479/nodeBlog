c262c3bc1898f4e4edc1d9d8f859958086ff55c6
Brendan Kelly <brendan.kelly4@marist.edu>
2018-05-12 21:45:10 -0400

    added login page data and database connection

--- socket-app/app.js
+++ socket-app/app.js
@@ -4,6 +4,16 @@ var app = express();
 var mysql = require('mysql');
 require('dotenv').config();
 
+var connection = mysql.createConnection({
+	
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
+})
+
+connection.connect();
+
 //use the public and views folders
 app.use(express.static('public'));
 app.use(express.static('views'));
